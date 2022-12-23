f53688413830d6d947e37786bec402db160b22b7
ParkJongHa <zoyngm@gmail.com>
2022-12-23 16:31:06 +0900

    sample009_aop

--- src/main/kotlin/com/example/demo/sample009_aop/Sample009Aop.kt
+++ src/main/kotlin/com/example/demo/sample009_aop/Sample009Aop.kt
@@ -5,10 +5,11 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
 import org.springframework.stereotype.Component
+import org.springframework.web.context.request.WebRequest
 
 @Component
 @Aspect
-class Sample009AopLogger {
+class Sample009Aop {
 
     @Pointcut("execution(* com..*Controller.*(..) )")
     fun controllerPointcut() {}
@@ -16,9 +17,17 @@ class Sample009AopLogger {
     @Around("controllerPointcut()")
     fun log(proceedingJoinPoint: ProceedingJoinPoint): Any? {
 
-        proceedingJoinPoint.args.forEach {
-            println("proceedingJoinPoint.args > $it")
-        }
+        proceedingJoinPoint.args.forEach {when (it) {
+            is WebRequest -> {
+                println("ProceedingJoinPoint.args is WebRequest > ${it.contextPath}")
+            }
+            is Throwable -> {
+                println("ProceedingJoinPoint.args is Throwable > ${it.printStackTrace()}")
+            }
+            else -> {
+                println("ProceedingJoinPoint.args is undefined > $it")
+            }
+        }}
 
         return proceedingJoinPoint.proceed()
     }
