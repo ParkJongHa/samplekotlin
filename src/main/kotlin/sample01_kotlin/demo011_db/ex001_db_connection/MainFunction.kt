6772b23ed6a8fe7f6e16933e7d48189876cc5546
Administrator <@>
2024-08-26 18:54:57 +0900

    db connection add

---
+++ src/main/kotlin/sample01_kotlin/demo011_db/ex001_db_connection/MainFunction.kt
@@ -0,0 +1,14 @@
+package sample01_kotlin.demo011_db.ex001_db_connection
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+fun main() {
+    val jdbcUrl = "jdbc:postgresql://localhost:5432/ltlk"
+    val username = "123"
+    val password = "456"
+
+    Class.forName("org.postgresql.Driver") // Register the PostgreSQL driver
+    val connection: Connection = DriverManager.getConnection(jdbcUrl, username, password) // Connect to the database
+    connection.close()
+}
