4ed6a551fe51b3e460405ca833f8c6c33170980c
Administrator <@>
2024-10-14 10:16:57 +0900

    lib version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     /* default */ kotlin("jvm") version "1.9.25"
     /* default */ kotlin("plugin.spring") version "2.0.21"
@@ -17,36 +15,36 @@ repositories {
 }
 
 dependencies {
-    /* default */ implementation("org.springframework.boot:spring-boot-starter-web")
-    /* default */ implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    /* default */ implementation("org.springframework.boot:spring-boot-starter-web:3.3.2")
+    /* default */ implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
     /* default */ implementation("org.jetbrains.kotlin:kotlin-reflect")
     /* default */ implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    /* default */ developmentOnly("org.springframework.boot:spring-boot-devtools")
-    /* default */ testImplementation("org.springframework.boot:spring-boot-starter-test")
+    /* default */ developmentOnly("org.springframework.boot:spring-boot-devtools:3.3.2")
+    /* default */ testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.1")
 
-    /* demo003_gson */ implementation("com.google.code.gson:gson:2.10")
+    /* demo003_gson */ implementation("com.google.code.gson:gson:2.10.1")
 
-    /* demo004_mapstruct */ kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
-    /* demo004_mapstruct */ kaptTest("org.mapstruct:mapstruct-processor:1.5.3.Final")
-    /* demo004_mapstruct */ implementation("org.mapstruct:mapstruct:1.5.3.Final")
-    /* demo004_mapstruct */ annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+    /* demo004_mapstruct */ kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    /* demo004_mapstruct */ kaptTest("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    /* demo004_mapstruct */ implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    /* demo004_mapstruct */ annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 
-    /* demo007_restful */ implementation("org.springframework.boot:spring-boot-starter-validation:2.7.1")
+    /* demo007_restful */ implementation("org.springframework.boot:spring-boot-starter-validation:3.3.2")
 
 
     /* demo008_multipartfile */ implementation("commons-fileupload:commons-fileupload:1.5")
     /* demo009_multipartfileimageresize */ implementation("org.imgscalr:imgscalr-lib:4.2")
 
-    /* demo016_quartz */ implementation("org.springframework.boot:spring-boot-starter-quartz")
+    /* demo016_quartz */ implementation("org.springframework.boot:spring-boot-starter-quartz:3.3.2")
     /* demo016_quartz */ implementation("org.quartz-scheduler:quartz:2.3.2")
 
-    /* demo019_rabbitmq */ implementation("org.springframework.boot:spring-boot-starter-amqp")
+    /* demo019_rabbitmq */ implementation("org.springframework.boot:spring-boot-starter-amqp:3.3.2")
 
-    /* demo020_jsoup */ implementation("org.jsoup:jsoup:1.15.3") // jsoup 크롤링
+    /* demo020_jsoup */ implementation("org.jsoup:jsoup:1.18.1") // jsoup 크롤링
 
-    /* demo021_h2 */ implementation("com.h2database:h2")
+    /* demo021_h2 */ implementation("com.h2database:h2:2.2.224")
 
-    /* demo022_jpa */ implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    /* demo022_jpa */ implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.2")
 
 //    /* demo026_slf4j */ implementation("org.springframework.boot:spring-boot-starter-log4j2")
 //    /* demo026_slf4j */ implementation("org.project-lombok:lombok:1.18.22")
@@ -59,10 +57,10 @@ dependencies {
 
 
 
-    /* kotlin coroutines */ implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    /* kotlin coroutines */ implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
 
-    /* demo004 og */implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    /* demo004 og */implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
 
 }
 
