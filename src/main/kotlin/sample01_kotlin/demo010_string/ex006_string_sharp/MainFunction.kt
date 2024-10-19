10daee9296d3e74941d7400290d533cb4b27248f
ParkJongHa <zoyngm@gmail.com>
2024-10-19 14:14:16 +0900

    remove last #

---
+++ src/main/kotlin/sample01_kotlin/demo010_string/ex006_string_sharp/MainFunction.kt
@@ -0,0 +1,21 @@
+
+package sample01_kotlin.demo010_string.ex006_string_sharp
+
+const val sample = "https://eopla.net/magazines/22222#"
+//const val sample = "https://eopla.net/magazines/22222##"
+//const val sample = "https://eopla.net/magazines/222#22?page=43&size=30"
+
+// 끝이 붙은 # 은 다 지운다.
+fun removeLastSharp(string: String): String {
+    var temp = string
+
+    while (temp.endsWith("#") && temp.lastIndexOf("#") == temp.lastIndex) {
+        temp = temp.substring(0, temp.lastIndexOf("#"))
+    }
+
+    return temp
+}
+
+fun main() {
+    println( removeLastSharp(sample) )
+}
