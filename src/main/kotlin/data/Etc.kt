9a7c9b38c865258efc9ce2f17685d570957e59af
Administrator <@>
2024-10-24 18:56:36 +0900

    file read

---
+++ src/main/kotlin/data/Etc.kt
@@ -0,0 +1,357 @@
+package data
+
+val categoryList = listOf(
+    "all"
+    ,"accounting-and-auditing"
+    ,"adult"
+    ,"advertising-and-marketing"
+    ,"aerospace-and-defense"
+    ,"agriculture"
+    ,"airlines"
+    ,"apparel-and-fashion"
+    ,"architecture"
+    ,"automotive"
+    ,"banking"
+    ,"beauty-and-cosmetics"
+    ,"biotech"
+    ,"ceramics-and-pottery"
+    ,"chemicals"
+    ,"civil-engineering"
+    ,"comics-and-animation"
+    ,"computer-and-video-games"
+    ,"computer-hardware"
+    ,"computer-software-and-development"
+    ,"computers-and-electronics"
+    ,"construction-and-maintenance"
+    ,"consulting"
+    ,"consumer-electronics"
+    ,"crafts"
+    ,"customer-services"
+    ,"design"
+    ,"distance-learning"
+    ,"ecology-and-environment"
+    ,"education"
+    ,"entertainment"
+    ,"equipment-and-supplies"
+    ,"events-services"
+    ,"facilities-services"
+    ,"farms-and-ranches"
+    ,"finance"
+    ,"fishery"
+    ,"food-and-beverages"
+    ,"foreign-language"
+    ,"furniture"
+    ,"gambling"
+    ,"government"
+    ,"graphic-design"
+    ,"healthcare"
+    ,"hospitality"
+    ,"human-resources"
+    ,"import-and-export"
+    ,"information-technology"
+    ,"insurance"
+    ,"investment"
+    ,"jewelry-and-luxury-products"
+    ,"legal"
+    ,"libraries"
+    ,"machinery"
+    ,"mail-and-package-delivery"
+    ,"manufacturing"
+    ,"maritime"
+    ,"market-research"
+    ,"mass-media"
+    ,"medical-devices-and-equipment"
+    ,"mental-health"
+    ,"metals-and-mining"
+    ,"military"
+    ,"museums"
+    ,"music"
+    ,"nanotechnology"
+    ,"newspapers"
+    ,"nonprofit-organizations"
+    ,"oil-and-gas"
+    ,"online-services"
+    ,"outsourcing"
+    ,"packaging-and-containers"
+    ,"performing-arts"
+    ,"pharmaceuticals"
+    ,"photography"
+    ,"plastics-and-polymers"
+    ,"political-organizations"
+    ,"printing"
+    ,"public-policy"
+    ,"public-relations"
+    ,"public-safety"
+    ,"public-utility"
+    ,"publishing"
+    ,"real-estate"
+    ,"recreational-facilities"
+    ,"recruitment-and-staffing"
+    ,"reference"
+    ,"religion-and-belief"
+    ,"renewable-energy"
+    ,"restaurants"
+    ,"retail"
+    ,"science"
+    ,"security-products-and-services"
+    ,"social-and-charitable-organizations"
+    ,"sporting-goods"
+    ,"sports"
+    ,"telecom"
+    ,"textiles-and-nonwovens"
+    ,"tobacco"
+    ,"training-and-certification"
+    ,"transportation-and-logistics"
+    ,"travel-and-tourism"
+    ,"tv-and-movies"
+    ,"venture-capital"
+    ,"veterinary"
+    ,"visual-art"
+    ,"warehousing"
+    ,"wellness"
+    ,"wholesalers-and-liquidators"
+    ,"wine-and-spirits"
+    ,"writing-and-editing-services"
+)
+val countryList = listOf(
+    "global"  // 전 세계
+    ,"us"  // 미국
+    ,"gb"  // 영국
+    ,"au"  // 호주
+    ,"fr"  // 프랑스
+    ,"ca"  // 캐나다
+    ,"es"  // 스페인
+    ,"in"  // 인도
+    ,"de"  // 독일
+    ,"it"  // 이탈리아
+    ,"br"  // 브라질
+    ,"ad"  // 안도라
+    ,"ae"  // 아랍 에미리트
+    ,"af"  // 아프가니스탄
+    ,"ag"  // 앤티가 바부다
+    ,"ai"  // 앵귈라
+    ,"al"  // 알바니아
+    ,"am"  // 아르메니아
+    ,"ao"  // 앙골라
+    ,"aq"  // 남극 대륙
+    ,"ar"  // 아르헨티나
+    ,"as"  // 아메리칸 사모아
+    ,"at"  // 오스트리아
+    ,"aw"  // 아루바
+    ,"ax"  // 올란드
+    ,"az"  // 아제르바이잔
+    ,"ba"  // 보스니아 헤르체고비나
+    ,"bb"  // 바베이도스
+    ,"bd"  // 방글라데시
+    ,"be"  // 벨기에
+    ,"bf"  // 부르키나 파소
+    ,"bg"  // 불가리아
+    ,"bh"  // 바레인
+    ,"bi"  // 부룬디
+    ,"bj"  // 베냉
+    ,"bl"  // 생바르텔르미
+    ,"bm"  // 버뮤다
+    ,"bn"  // 브루나이
+    ,"bo"  // 볼리비아
+    ,"bq"  // 보네르, 신트외스타티위스 섬 및 사바
+    ,"bs"  // 바하마
+    ,"bt"  // 부탄
+    ,"bv"  // 부베 섬
+    ,"bw"  // 보츠와나
+    ,"by"  // 벨라루스
+    ,"bz"  // 벨리즈
+    ,"cc"  // 코코스 제도
+    ,"cd"  // 콩고 민주 공화국
+    ,"cf"  // 중앙아프리카 공화국
+    ,"cg"  // 콩고 공화국
+    ,"ch"  // 스위스
+    ,"ci"  // 코트디부아르
+    ,"ck"  // 쿡 제도
+    ,"cl"  // 칠레
+    ,"cm"  // 카메룬
+    ,"cn"  // 중국
+    ,"co"  // 콜롬비아
+    ,"cr"  // 코스타리카
+    ,"cv"  // 카보베르데
+    ,"cw"  // 퀴라소
+    ,"cx"  // 크리스마스 섬
+    ,"cy"  // 키프로스
+    ,"cz"  // 체코 공화국
+    ,"dj"  // 지부티
+    ,"dk"  // 덴마크
+    ,"dm"  // 도미니카 연방
+    ,"do"  // 도미니카 공화국
+    ,"dz"  // 알제리
+    ,"ec"  // 에콰도르
+    ,"ee"  // 에스토니아
+    ,"eg"  // 이집트
+    ,"eh"  // 서사하라
+    ,"er"  // 에리트레아
+    ,"et"  // 에티오피아
+    ,"fi"  // 핀란드
+    ,"fj"  // 피지
+    ,"fk"  // 포클랜드 제도
+    ,"fm"  // 미크로네시아 연방
+    ,"fo"  // 페로스 제도
+    ,"ga"  // 가봉
+    ,"gd"  // 그레나다
+    ,"ge"  // 조지아
+    ,"gf"  // 프랑스령 기아나
+    ,"gg"  // 건지 섬
+    ,"gh"  // 가나
+    ,"gi"  // 지브롤터
+    ,"gl"  // 그린란드
+    ,"gm"  // 감비아
+    ,"gn"  // 기니
+    ,"gp"  // 과들루프
+    ,"gq"  // 적도 기니
+    ,"gr"  // 그리스
+    ,"gs"  // 사우스조지아 사우스샌드위치 제도
+    ,"gt"  // 과테말라
+    ,"gu"  // 괌
+    ,"gw"  // 기니비사우
+    ,"gy"  // 가이아나
+    ,"hk"  // 홍콩
+    ,"hm"  // 허드 맥도널드 제도
+    ,"hn"  // 온두라스
+    ,"hr"  // 크로아티아
+    ,"ht"  // 아이티
+    ,"hu"  // 헝가리
+    ,"id"  // 인도네시아
+    ,"ie"  // 아일랜드
+    ,"il"  // 이스라엘
+    ,"im"  // 맨섬
+    ,"io"  // 영국령 인도양 지역
+    ,"iq"  // 이라크
+    ,"is"  // 아이슬란드
+    ,"je"  // 저지 섬
+    ,"jm"  // 자메이카
+    ,"jo"  // 요르단
+    ,"jp"  // 일본
+    ,"ke"  // 케냐
+    ,"kg"  // 키르기스스탄
+    ,"kh"  // 캄보디아
+    ,"ki"  // 키리바시
+    ,"km"  // 코모로
+    ,"kn"  // 세인트키츠 네비스
+    ,"kr"  // 대한민국
+    ,"kw"  // 쿠웨이트
+    ,"ky"  // 케이맨 제도
+    ,"kz"  // 카자흐스탄
+    ,"la"  // 라오스
+    ,"lb"  // 레바논
+    ,"lc"  // 세인트루시아
+    ,"li"  // 리히텐슈타인
+    ,"lk"  // 스리랑카
+    ,"lr"  // 라이베리아
+    ,"ls"  // 레소토
+    ,"lt"  // 리투아니아
+    ,"lu"  // 룩셈부르크
+    ,"lv"  // 라트비아
+    ,"ma"  // 모로코
+    ,"mc"  // 모나코
+    ,"md"  // 몰도바
+    ,"me"  // 몬테네그로
+    ,"mf"  // 세인트마틴 섬
+    ,"mg"  // 마다가스카르
+    ,"mh"  // 마셜 제도
+    ,"mk"  // 북마케도니아
+    ,"ml"  // 말리
+    ,"mn"  // 몽골
+    ,"mo"  // 마카오
+    ,"mp"  // 북마리아나 제도
+    ,"mq"  // 마르티니크
+    ,"mr"  // 모리타니
+    ,"ms"  // 몬트세랫
+    ,"mt"  // 몰타
+    ,"mu"  // 모리셔스
+    ,"mv"  // 몰디브
+    ,"mw"  // 말라위
+    ,"mx"  // 멕시코
+    ,"my"  // 말레이시아
+    ,"mz"  // 모잠비크
+    ,"na"  // 나미비아
+    ,"nc"  // 누벨칼레도니(뉴칼레도니아)
+    ,"ne"  // 니제르
+    ,"nf"  // 노퍽 섬
+    ,"ng"  // 나이지리아
+    ,"ni"  // 니카라과
+    ,"nl"  // 네덜란드
+    ,"no"  // 노르웨이
+    ,"np"  // 네팔
+    ,"nr"  // 나우루
+    ,"nu"  // 니우에
+    ,"nz"  // 뉴질랜드
+    ,"om"  // 오만
+    ,"pa"  // 파나마
+    ,"pe"  // 페루
+    ,"pf"  // 프랑스령 폴리네시아
+    ,"pg"  // 파푸아뉴기니
+    ,"ph"  // 필리핀
+    ,"pk"  // 파키스탄
+    ,"pl"  // 폴란드
+    ,"pm"  // 생피에르 미클롱
+    ,"pr"  // 푸에르토리코
+    ,"ps"  // 팔레스타인
+    ,"pt"  // 포르투갈
+    ,"pw"  // 팔라우
+    ,"py"  // 파라과이
+    ,"qa"  // 카타르
+    ,"re"  // 레위니옹
+    ,"ro"  // 루마니아
+    ,"rs"  // 세르비아
+    ,"ru"  // 러시아
+    ,"rw"  // 르완다
+    ,"sa"  // 사우디아라비아
+    ,"sb"  // 솔로몬 제도
+    ,"sc"  // 세이셸
+    ,"se"  // 스웨덴
+    ,"sg"  // 싱가포르
+    ,"sh"  // 세인트헬레나
+    ,"si"  // 슬로베니아
+    ,"sj"  // 스발바르 얀마옌 제도
+    ,"sk"  // 슬로바키아
+    ,"sl"  // 시에라리온
+    ,"sm"  // 산마리노
+    ,"sn"  // 세네갈
+    ,"so"  // 소말리아
+    ,"sr"  // 수리남
+    ,"ss"  // 남수단
+    ,"st"  // 상투메 프린시페
+    ,"sv"  // 엘살바도르
+    ,"sx"  // 신트마르턴
+    ,"sz"  // 스와질란드
+    ,"tc"  // 터크스 케이커스 제도
+    ,"td"  // 차드
+    ,"tg"  // 토고
+    ,"th"  // 태국
+    ,"tj"  // 타지키스탄
+    ,"tk"  // 토켈라우
+    ,"tl"  // 동티모르
+    ,"tm"  // 투르크메니스탄
+    ,"tn"  // 튀니지
+    ,"to"  // 통가
+    ,"tr"  // 터키
+    ,"tt"  // 트리니다드 토바고
+    ,"tv"  // 투발루
+    ,"tw"  // 대만
+    ,"tz"  // 탄자니아
+    ,"ua"  // 우크라이나
+    ,"ug"  // 우간다
+    ,"uy"  // 우루과이
+    ,"uz"  // 우즈베키스탄
+    ,"va"  // 바티칸 시국
+    ,"vc"  // 세인트빈센트 그레나딘
+    ,"ve"  // 베네수엘라
+    ,"vg"  // 영국령 버진아일랜드
+    ,"vi"  // 미국령 버진아일랜드
+    ,"vn"  // 베트남
+    ,"vu"  // 바누아투
+    ,"wf"  // 왈리스 푸투나 제도
+    ,"ws"  // 사모아
+    ,"ye"  // 예멘
+    ,"yt"  // 마요트
+    ,"za"  // 남아프리카 공화국
+    ,"zm"  // 잠비아
+    ,"zw"  // 짐바브웨
+)
---
+++ src/main/kotlin/sample01_kotlin/demo013_file/ex001_read/FileReadMainFunction.kt
@@ -0,0 +1,110 @@
+package sample01_kotlin.demo013_file.ex001_read
+
+import data.categoryList
+import data.countryList
+import org.jsoup.Jsoup
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import kotlin.random.Random
+
+fun main() {
+//    domainExtract()
+//    combination()
+    getDomainSet()
+}
+
+fun domainExtract() {
+    val file = File("C:\\Users\\name\\a.html")
+    val reader = BufferedReader(FileReader(file, Charsets.UTF_8))
+//    reader.lines().forEach { println(it) }
+
+    var splittedLine: List<String>
+    var splittedRaw: List<String>
+    val domainList = mutableSetOf<String>()
+
+    reader.lines().forEach { aLine ->
+        splittedLine = aLine.split("data-cl=\"true\">")
+
+        if (1 < splittedLine.size) {
+            splittedLine.forEach{ aRaw ->
+                splittedRaw = aRaw.split("</a></div></td>")
+                if (1 < splittedRaw.size) {
+                    domainList.add( splittedRaw[0] )
+                }
+            }
+        }
+    }
+
+//    domainList.forEach { println(it) }
+}
+
+fun combination() {
+    var totalCount = 0
+    countryList.forEach { categoryList.forEach { totalCount++ } }
+
+    var index = 0
+    countryList.forEach { aCountry ->
+        categoryList.forEach { aCategory ->
+            index++
+            println("${"$index".padStart(totalCount.toString().length, '0')} $aCountry/$aCategory")
+        }
+    }
+}
+
+fun getDomainSet() {
+    val domainSet = mutableSetOf<String>()
+
+    countryList.forEach { aCountry ->
+        categoryList.filter { it != "adult" }.forEach { aCategory ->
+            val start = System.currentTimeMillis()
+            println("$aCountry $aCategory start")
+
+            urlExtract(aCountry, aCategory, domainSet)
+
+            println("$aCountry $aCategory delay (${System.currentTimeMillis() - start}ms)")
+            Thread.sleep(1000L + Random.nextLong(1000))
+        }
+    }
+
+    domainSet.forEach { println(it) }
+}
+
+fun urlExtract(countryCode: String, category: String, domainSet: MutableSet<String>) {
+    try {
+        val document = Jsoup.connect("$countryCode/$category").get()
+
+        val section = document.childNodes()[4]
+            .childNodes()[3]
+            .childNodes()[3]
+            .childNodes()[15]
+            .childNodes()[1]
+            .childNodes()[5]
+            .childNodes()[0]
+            .childNodes()[2]
+            .childNodes()[0]
+            .childNodes()[1]
+
+        val tbody = section
+            .childNodes()[1]
+            .childNodes()[0]
+            .childNodes()[0]
+            .childNodes()[0]
+            .childNodes()[1]
+
+        tbody.childNodes().forEach { tr ->
+            try {
+                val node = tr.childNodes()[1]
+                    .childNodes()[0]
+                    .childNodes()[1]
+                    .childNodes()[0]
+
+                domainSet.add( node.outerHtml() )
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
